b9ae4dd215d2a4b1e0f253b10525e3659f5a7cb6
Mu Qiao <qiaomuf@gentoo.org>
2011-05-31 21:27:47 +0800

    Utility: add more portage functions

--- utils/isolated-functions.sh
+++ utils/isolated-functions.sh
@@ -20,3 +20,35 @@ EXPORT_FUNCTIONS() {
     fi   
     $__export_funcs_var="$__export_funcs_var $*" 
 }
+
+use() {
+    echo "use should be called"
+    return 1
+}
+
+useq() {
+    echo "useq should be called"
+    return 1
+}
+
+use_with() {
+    echo "use_with should be called"
+    return 1
+}
+
+use_enable() {
+    echo "use_enable should be called"
+    return 1
+}
+
+eerror() {
+    echo "eerror: $*" >&2
+}
+
+debug-print() {
+    echo "debug-print: $*"
+}
+
+ewarn() {
+    echo "ewarn: $*" >&2
+}
